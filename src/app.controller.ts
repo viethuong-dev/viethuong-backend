02ccc0cba6f6ecd1f6c3fd45a5741aa6f8a09a74
buiminhhieuno5@gmail.com <buiminhhieuno5@gmail.com>
2023-08-20 11:26:51 +0700

    chore: Setup nginx

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Public } from './guards/base.guard';
 
 @Controller()
 export class AppController {
     constructor(private readonly appService: AppService) {}
 
+    @Public()
     @Get()
-    getHello(): string {
-        return this.appService.getHello();
+    ping(): object {
+        return {};
     }
 }
